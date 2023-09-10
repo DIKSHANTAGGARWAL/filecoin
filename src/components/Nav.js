20c82a3d801dea858bacffee01954b554fcaa1fc
DIKSHANTAGGARWAL <125082477+DIKSHANTAGGARWAL@users.noreply.github.com>
2023-09-10 23:02:24 +0530

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,44 +2,48 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const Nav = () => {
-    const [check, setCheck] =useState(null);
+    // var [check, setCheck] =useState();
     const auth = localStorage.getItem('user');
     const navigate = useNavigate();
     const logout = () => {
         localStorage.clear();
-        navigate('/signup')
+        navigate('/login')
     }
-    
-        if(typeof(localStorage.getItem('user'))!='undefined'){
-            var result= localStorage.getItem("user");
-            const res=JSON.parse(result);
-        
-        
-            console.log(res.user.email)
-            if(res.user.email==='admin@gmail.com')
-            {
-                // console.log("ok")
-                setCheck(1);
-            }
+
+
+    if (localStorage.getItem('user')) {
+        var result = localStorage.getItem("user");
+        const res = JSON.parse(result)
+        var check = 0
+        // console.log(res.email)
+        if (res.email == 'admin@gmail.com') {
+            console.log("ok")
+            check = 1
         }
-    console.log(check)
-    
+    }
+
+    // console.log(check)
+
     return (
         <div className="navbar">
             <img alt="d.a.r.k"
-            className="logo"
-             src="https://avatars.githubusercontent.com/u/105876150?s=280&v=4"/>
-            {auth ? <ul className="nav-ul"> 
+                className="logo"
+                src="https://avatars.githubusercontent.com/u/105876150?s=280&v=4" />
+            {auth ? <ul className="nav-ul">
                 <li><Link to="/metamask">Metamask</Link></li>
-                <li><Link to="/products">Candidates</Link></li>
-                   { check==1 &&  <>
-                    <li><Link to="/onlyAdminProducts">AdminCandidates</Link></li>
-                    <li><Link to="/add">Add Candidate</Link></li></> 
-                    }
-                
+                {check == 0 ? <>
+                    <li><Link to="/products">Candidates</Link></li>
+                </> : <></>
+                }
+                {check == 1 ? <>
+                    <li><Link to="/onlyAdminProducts">Candidates</Link></li>
+                    <li><Link to="/add">Add Candidate</Link></li>
+                </> : <></>
+                }
+
                 {/* <li><Link to="/update">Update</Link></li> */}
                 {/* <li><Link to="/logout">Logout</Link></li> */}
-                <li> <Link onClick={logout} to="/signup">Logout {JSON.parse(auth).name}</Link></li>
+                <li> <Link onClick={logout} to="/login">Logout ({JSON.parse(auth).name})</Link></li>
             </ul>
                 :
                 <ul className="nav-ul nav-right">
